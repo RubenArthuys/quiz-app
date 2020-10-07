f5aa94dd23ac28b448e96efe5e984bd104217610
RubenArthuys <ruben.arthuys@gmail.com>
2020-10-07 11:50:51 +0200

    Updates

---
+++ precache-manifest.062f9ba5f7eae43dd666bf50fb50d8e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ce0da399ed0c9f4b609c9d8e9f0d22",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "34178ba62713a406862e",
+    "url": "/quiz-app/static/css/main.64f248fe.chunk.css"
+  },
+  {
+    "revision": "97b8cb1fdfd0a2509bb3",
+    "url": "/quiz-app/static/js/2.5abaa134.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.5abaa134.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34178ba62713a406862e",
+    "url": "/quiz-app/static/js/main.060e7038.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  }
+]);
